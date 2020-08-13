6aef2cfbb573a67045ab773c0e4a031fa17edcf7
Carlos Alexandre Ribeiro <carlosferreira.jlle@gmail.com>
2020-08-13 00:43:16 -0300

    Adicionado banco de dados SQLite

--- src/server.js
+++ src/server.js
@@ -1,105 +1,34 @@
-const proffys = [
-    { 
-        name:"Carlos Alexandre", 
-        avatar: "https://avatars1.githubusercontent.com/u/52242005?s=460&u=9a1a6659173ebb76e8a4b9a4d64164ddefb17e85&v=4", 
-        whatsapp: 47992107826, 
-        bio: "Entusiasta das melhores tecnologias.<br><br>Apaixonado por explodir coisas", 
-        subject: "Quimica", 
-        cost: "80", 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    },
-
-    { 
-        name:"Henrique Silva", 
-        avatar: "https://avatars0.githubusercontent.com/u/39440032?s=460&u=8a02e6b17a45ca9bdbfbe70bf91e0ad9404421ac&v=4", 
-        whatsapp: 47992107826, 
-        bio: "Entusiasta das melhores tecnologias.<br><br>Apaixonado por explodir coisas", 
-        subject: "Quimica", 
-        cost: "80", 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    }
-
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-
-]
-//Funcionalidades
-
-function getSubject(subjectNumber){
-    const arrayPosition = +subjectNumber - 1
-    return subjects[arrayPosition]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-     const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays }) 
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    //se tiver data
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty){
-
-        data.subject = getSubject(data.subject)
-
-         //adicionar data a lista de proffys
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    // se não mostrar a página
-    return res.render("give-classes.html", { subjects, weekdays }) 
-}
-
 //Servidor
 const express = require('express')
 const server = express()
+
+const { 
+    pageLanding, 
+    pageStudy, 
+    pageGiveClasses, 
+    saveClasses
+} = require('./pages')
+
+
+//configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
-
-
-//configurar nunjucks
+//const e = require('express')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
+//inicio e configuração do servidor
 server
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true}))
 // configurar arquivos estátivos (css, scripts, imagens)
 .use(express.static("public"))
 //rotas das aplicações
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//start do servidor
 .listen(5500)
 
